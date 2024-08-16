74abdccb69be85326defca2abbba2fe473bbb652
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-08-16 16:13:53 +0100

    step-5 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Option from "./components/Option/Option";
 
 export default function App() {
-  const [count, setCount] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [count, setCount] = useState(() => {
+    const savedState = localStorage.getItem("feedbackState");
+    if (savedState) {
+      return JSON.parse(savedState);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
   });
+
+  useEffect(() => {
+    localStorage.setItem("feedbackState", JSON.stringify(count));
+  }, [count]);
 
   const updateFeedback = (feedbackType) => {
     if (feedbackType === "reset") {
